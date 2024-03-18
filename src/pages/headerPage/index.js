47da04dd2b92c136395a55753310bccb0caf31f0
spring1chong <spring1chong@sjtu.edu.cn>
2024-03-18 21:28:44 +0800

    为header添加了最小宽度

--- src/pages/headerPage/index.js
+++ src/pages/headerPage/index.js
@@ -11,16 +11,16 @@ const onSearch = (value, _e, info) => console.log(info?.source, value);
 
 const Header = () => {
 
-  const {login}=useSelector(state=>state.login)
- 
-  return (
-    <div className="flex justify-between items-center bg-blue-600 text-white h-20 rounded-2xl ">
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
-        <style>{`
+	const { login } = useSelector(state => state.login)
+
+	return (
+		<div className="flex justify-between items-center bg-blue-600 text-white h-20 min-w-[800px] rounded-2xl ">
+			<Helmet>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+				/>
+				<style>{`
           .material-symbols-outlined {
              font-variation-settings:
              'FILL' 0,
@@ -30,22 +30,22 @@ const Header = () => {
              font-size: 80px;
            }
         `}</style>
-      </Helmet>
-      <Link  to={{ pathname: '/'}}><div className="flex items-center cursor-pointer"  >
-        <img src={logo} alt="logo" className="h-20 rounded-2xl" />
-        <span className="ml-4 text-4xl">交∩集</span>
-      </div></Link>
-      <Search
-        placeholder="请输入感兴趣的话题或帖子"
-        onSearch={onSearch}
-        style={{
-          width: 400,
-        }}
-        size="large"
-      />
-      <Login/>
-    </div>
-  );
+			</Helmet>
+			<Link to={{ pathname: '/' }}><div className="flex items-center cursor-pointer"  >
+				<img src={logo} alt="logo" className="h-20 rounded-2xl" />
+				<span className="ml-4 text-4xl">交∩集</span>
+			</div></Link>
+			<Search
+				placeholder="请输入感兴趣的话题或帖子"
+				onSearch={onSearch}
+				style={{
+					width: 400,
+				}}
+				size="large"
+			/>
+			<Login />
+		</div>
+	);
 };
 
 export default Header;
