bfb950700fad7444f8f6103b028a581f4a54bf67
SundayChen <824987073@qq.com>
2024-03-29 19:29:31 +0800

    修改了比例

--- src/components/remark/objectProfile.js
+++ src/components/remark/objectProfile.js
@@ -2,7 +2,7 @@ import restaurantPhoto from "@/assets/restaurant.jpg";
 
 const ObjectProfile = ({object}) => {
     return (
-        <div className="flex flex-col items-center max-w-md h-fit bg-white rounded-lg drop-shadow-lg px-12 py-8 mt-12 mb-0">
+        <div className="flex flex-col items-center w-1/4 h-fit bg-white rounded-lg drop-shadow-lg px-12 py-8 mt-12 ml-6">
             <h2 className="text-3xl mb-5 font-thin">{object.name}</h2>
             <img src={restaurantPhoto} alt="restaurantPhoto" />
             <p className="mt-6">{object.description}</p>
