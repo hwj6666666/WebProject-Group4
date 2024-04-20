16c0a359ffbadfb929b8aff2ceb6ba689e64c53c
HuWenjie <3289915620@qq.com>
2024-04-20 21:06:41 +0800

    start the trip of backend truly

---
+++ TechPrototype/jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package org.example.jiaoji.mapper;
+
+public interface UserMapper {
+}
---
+++ TechPrototype/jiaoji_backend/jiaoji/src/test/java/org/example/jiaoji/JiaojiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jiaoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiaojiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
