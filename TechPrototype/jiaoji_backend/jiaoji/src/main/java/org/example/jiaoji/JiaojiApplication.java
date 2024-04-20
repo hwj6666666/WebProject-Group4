69c7ecaa54f4a644887f5cf2fb8e46091c38d393
SundayChen <824987073@qq.com>
2024-04-20 21:00:32 +0800

    backend

---
+++ TechPrototype/jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/JiaojiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jiaoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiaojiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiaojiApplication.class, args);
+    }
+
+}
---
+++ TechPrototype/jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/pojo/User.java
@@ -0,0 +1,16 @@
+package org.example.jiaoji.pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String avatar;
+    private String note;
+}
