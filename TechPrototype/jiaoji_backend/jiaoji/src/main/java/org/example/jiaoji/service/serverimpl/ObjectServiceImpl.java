8d2d20794bbcc53b8a661427f84e959cad839738
HuWenjie <3289915620@qq.com>
2024-05-13 17:27:22 +0800

    fix the object bugs in the backend

--- TechPrototype/jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/service/serverimpl/ObjectServiceImpl.java
+++ TechPrototype/jiaoji_backend/jiaoji/src/main/java/org/example/jiaoji/service/serverimpl/ObjectServiceImpl.java
@@ -1,77 +1,83 @@
 package org.example.jiaoji.service.serverimpl;
 
+import java.util.List;
 import org.example.jiaoji.mapper.ObjectMapper;
+import org.example.jiaoji.pojo.Objects;
 import org.example.jiaoji.pojo.Remark;
 import org.example.jiaoji.pojo.RetType;
 import org.example.jiaoji.pojo.Topic;
-import org.example.jiaoji.pojo.Objects;
 import org.example.jiaoji.service.ObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ObjectServiceImpl implements ObjectService {
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired private ObjectMapper objectMapper;
 
-    public RetType InsertObject(Objects data) {
-        RetType ret = new RetType();
+  public RetType InsertObject(Objects data) {
+    RetType ret = new RetType();
 
-        Integer id = objectMapper.selectIdByTitle(data.getTitle(), data.getTopicId());
-        if(id !=null) {
-            ret.setMsg("该对象已存在");
-            ret.setOk(false);
-            ret.setData(null);
-            return ret;
-        }
-        System.out.println(data);
-        System.out.println("=======this is test=====");
-        Objects object = new Objects();
-        object.setTitle(data.getTitle());
-        object.setId(data.getId());
-        object.setDescription(data.getDescription());
-        object.setUserId(data.getUserId());
-        object.setPicture("this is topic picture");
-        object.setTopicId(data.getTopicId());
+    Integer id = objectMapper.selectIdByTitle(data.getTitle(), data.getTopicId());
+    if (id != null) {
+      ret.setMsg("该对象已存在");
+      ret.setOk(false);
+      ret.setData(null);
+      return ret;
+    }
+    System.out.println(data);
+    System.out.println("=======this is test=====");
+    Objects object = new Objects();
+    object.setTitle(data.getTitle());
+    object.setId(data.getId());
+    object.setDescription(data.getDescription());
+    object.setUserId(data.getUserId());
+    object.setPicture(data.getPicture());
 
-        objectMapper.insert(object);
-        ret.setMsg("上传成功");
+    object.setTopicId(data.getTopicId());
 
-        ret.setOk(true);
-        ret.setData(null);
-        return ret;
-    }
+    objectMapper.insert(object);
+    ret.setMsg("上传成功");
 
-    public List<Objects> SelectAllInTopic(Integer id){
-        return objectMapper.selectAllInTopic(id);
+    ret.setOk(true);
+    ret.setData(null);
+    return ret;
+  }
+
+  public List<Objects> SelectAllInTopic(Integer id) {
+    return objectMapper.selectAllInTopic(id);
+  }
+
+  public List<Objects> SelectById(Integer id) {
+    return objectMapper.selectById(id);
+  }
+
+  public List<Topic> SelectTopicById(Integer id) {
+    return objectMapper.selectTopicById(id);
+  }
+
+  public double getAveScore(Integer id) {
+    List<Remark> remarks = objectMapper.selectAllRemarks(id);
+    if (remarks != null && remarks.size() == 0) {
+      return 0;
     }
-    public  List<Objects> SelectById(Integer id){
-        return objectMapper.selectById(id);
+    Integer length = remarks.size();
+    double scores = 0;
+    for (Remark remark : remarks) {
+      scores += remark.getScore();
     }
-    public List<Topic> SelectTopicById(Integer id){
-        return objectMapper.selectTopicById(id);
+    return scores / length;
+  }
+
+  public String getHottestRemark(Integer id) {
+    List<Remark> remarks = objectMapper.selectAllRemarks(id);
+    Integer likes = 0;
+    String hottestRemark = "";
+    for (Remark remark : remarks) {
+      if (remark.getLike() > likes) {
+        likes = remark.getLike();
+        hottestRemark = remark.getContent();
+      }
     }
-    public double getAveScore(Integer id ){
-        List<Remark> remarks = objectMapper.selectAllRemarks(id);
-        Integer length = remarks.size();
-        double scores = 0;
-        for(Remark remark : remarks){
-            scores += remark.getScore();
-        }
-        return scores / length;
-    }
-    public String getHottestRemark(Integer id){
-        List<Remark> remarks = objectMapper.selectAllRemarks(id);
-        Integer likes = 0;
-        String hottestRemark = "";
-        for(Remark remark : remarks){
-            if(remark.getLike() > likes){
-                likes = remark.getLike();
-                hottestRemark = remark.getContent();
-            }
-        }
-        return hottestRemark;
-    }
+    return hottestRemark;
+  }
 }
