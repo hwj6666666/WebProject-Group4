44c073ecd34011f846450b7bda9bf4e4f017b033
spring1chong <spring1chong@sjtu.edu.cn>
2024-06-14 22:17:02 +0800

    click avatar to userpage

--- UIPrototype/src/components/user/UserCard.js
+++ UIPrototype/src/components/user/UserCard.js
@@ -4,23 +4,23 @@ import { Avatar } from "antd";
 import { Link } from "react-router-dom";
 import avat from "@/assets/3000.png";
 
-const UserCard = ({user}) => {
-	const { username,note,level,avatar } = user;
-    let tmpnote=note;
-    if(tmpnote.length>20){
-        tmpnote=tmpnote.substring(0,20)+"..."
-    }
-	return (<><Link to={{ pathname: `/user` }}>  
-        <div style={{display:"flex"}}>
-          <div>
-         <Avatar src={avatar!=="path"? avatar : avat} size={128} />
-		    </div>
-            <div>
-            <h1 className="ml-10 text-2xl"><span className="mr-5">{username}</span>{"等级："+level}</h1>
-            <p className="ml-10 text-xl text-black">{tmpnote}</p>
-            </div>
-        </div></Link>
-        </>
+const UserCard = ({ user }) => {
+	const { username, note, level, avatar } = user;
+	let tmpnote = note;
+	if (tmpnote.length > 20) {
+		tmpnote = tmpnote.substring(0, 20) + "..."
+	}
+	return (<><Link to={{ pathname: `/user/${user.id}` }}>
+		<div style={{ display: "flex" }}>
+			<div>
+				<Avatar src={avatar !== "path" ? avatar : avat} size={128} />
+			</div>
+			<div>
+				<h1 className="ml-10 text-2xl"><span className="mr-5">{username}</span>{"等级：" + level}</h1>
+				<p className="ml-10 text-xl text-black">{tmpnote}</p>
+			</div>
+		</div></Link>
+	</>
 	);
 };
 
